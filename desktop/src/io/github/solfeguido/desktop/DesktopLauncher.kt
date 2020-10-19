56b61fca20d3e96eda175e18d41f46a6c0b20361
AzariasB <azarias.boutin@probance.com>
2020-10-19 18:14:51 +0200

    Focusing on desktop development Updated to lwjgl3 backend

--- desktop/src/io/github/solfeguido/desktop/DesktopLauncher.kt
+++ desktop/src/io/github/solfeguido/desktop/DesktopLauncher.kt
@@ -1,17 +1,15 @@
 package io.github.solfeguido.desktop
 
-import com.badlogic.gdx.backends.lwjgl.LwjglApplication
-import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration
 import io.github.solfeguido.SolfeGuido
 
 object DesktopLauncher {
     @JvmStatic
     fun main(arg: Array<String>) {
-        val config = LwjglApplicationConfiguration()
-        config.title = "SolfeGuido"
-        config.width = 1280
-        config.height = 720
-        config.resizable = false
-        LwjglApplication(SolfeGuido(), config)
+        val config = Lwjgl3ApplicationConfiguration()
+        config.setTitle("SolfeGuido")
+        config.setWindowSizeLimits(1280, 720, 1280, 720)
+        Lwjgl3Application(SolfeGuido(), config)
     }
 }
