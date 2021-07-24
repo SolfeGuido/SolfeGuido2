e81e82abba754cf7487bc3762ab3a832c183e993
AzariasB <azarias.boutin@probance.com>
2021-07-24 14:54:43 +0200

    Update versions

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,10 +1,16 @@
 object Versions {
+    // App name
     const val SOLFEGUIDO = "Solfeguido"
+    // App version
     const val SOLFEGUIDO_VERION = "2.0.0-SNAPSHOT"
+    // Main language used for the app
     const val KOTLIN = "1.5.20"
+    // Used to serialized internal object into json, allowing easy saves
     const val KOTLIN_SERIALIZATION = "1.2.2"
+    // Used when loading the assets, to load them faster
     const val KOTLIN_COROUTINES = "1.5.0"
+    // Main lib to handle graphics, input, sound etc
     const val GDX = "1.10.0"
+    // Wrapper around libGdx to make it easier to use with kotlin
     const val KTX = "1.10.0-b1"
-    const val ROBO_VM = "2.3.13"
 }
