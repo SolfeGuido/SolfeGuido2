9d662fd7cd59389231d93cf8d0768ec5d6a3de83
AzariasB <azarias.boutin@probance.com>
2021-08-26 11:49:08 +0200

    Up kotlin version

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -4,7 +4,7 @@ object Versions {
     // App version
     const val APP_VERION = "2.0.0-SNAPSHOT"
     // Main language used for the app
-    const val KOTLIN = "1.5.21"
+    const val KOTLIN = "1.5.30"
     // Used to serialized internal object into json, allowing easy saves
     const val KOTLIN_SERIALIZATION = "1.2.2"
     // Used when loading the assets, to load them faster
