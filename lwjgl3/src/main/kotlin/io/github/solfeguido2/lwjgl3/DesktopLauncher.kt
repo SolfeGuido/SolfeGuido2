09d295251f39474434aa297db2ec1114608ec83d
AzariasB <azarias.boutin@probance.com>
2021-08-31 11:35:21 +0200

    Allow resize Desktop launcher to test android layouts

--- lwjgl3/src/main/kotlin/io/github/solfeguido2/lwjgl3/DesktopLauncher.kt
+++ lwjgl3/src/main/kotlin/io/github/solfeguido2/lwjgl3/DesktopLauncher.kt
@@ -7,6 +7,6 @@ import io.github.solfeguido2.SolfeGuido
 fun main(arg: Array<String>) {
     val config = Lwjgl3ApplicationConfiguration()
     config.setTitle("SolfeGuido")
-    config.setWindowSizeLimits(1280, 720, 1280, 720)
+    config.setWindowSizeLimits(800, 480, 1280, 720)
     Lwjgl3Application(SolfeGuido(), config)
 }
